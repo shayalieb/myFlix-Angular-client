696508b98a7ea5e039935b4336fd215595d83f72
shayalieb <shayalieberman@gmail.com>
2023-08-04 11:21:05 -0400

    all content has been added

--- src/app/movie-info-component/movie-info-component.component.ts
+++ src/app/movie-info-component/movie-info-component.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-movie-info-component',
   templateUrl: './movie-info-component.component.html',
-  styleUrls: ['./movie-info-component.component.css']
+  styleUrls: ['./movie-info-component.component.scss']
 })
 export class MovieInfoComponentComponent {
+  //@inject will add the data form the movie object the movie info dialog
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: { title: string, content: string}
+  ) { }
 
+  ngOnInt(): void { }
 }
